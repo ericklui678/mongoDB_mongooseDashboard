6d036e45f1cedf7b52c11e085731edb83f187723
Erick Lui <erickjin.lui@gmail.com>
2017-06-10 14:17:58 -0700

    reduced post route code when creating and updating

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    port = 3000;
 var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-
 mongoose.connect('mongodb://localhost/mongoose_db');
+
 var MongooseSchema = new mongoose.Schema({
     name: { type: String, required: [true, 'name required'], minlength: 2 },
     location: { type: String, required: [true, 'location required'], minlength: 3 },
@@ -14,65 +15,52 @@ var MongooseSchema = new mongoose.Schema({
     color: { type: String, required: [true, 'color required'], minlength: 3 }
 }, { timestamps: true });
 
-mongoose.model('Mongoose', MongooseSchema);
-var Mongoose = mongoose.model('Mongoose');
-mongoose.Promise = global.Promise;
+var Mongoose = mongoose.model('Mongoose', MongooseSchema);
 
+// Index
 app.get('/', function(req, res){
-    Mongoose.find(function(err, mongooses){
-        if(mongooses){
-            res.render('index', {mongooses: mongooses});
-        }
-        if(err){
-            console.log('Could not find any mongooses');
-            res.render('index');
-        }
+    Mongoose.find(function(err, data){
+        if(err) {console.log(err);}
+        res.render('index', {mongooses: data})
     }).sort({'createdAt': -1})
 })
+// Create form
 app.get('/new', function(req, res){
     res.render('new');
 })
+// Show info
 app.get('/mongooses/:id', function(req, res){
     Mongoose.find({_id: req.params.id}, function(err, mongooses){
         res.render('info', {mongooses: mongooses[0]});
     })
 })
+// Edit form
 app.get('/mongooses/edit/:id', function(req, res){
     console.log(req.params.id);
     res.render('edit', {id: req.params.id})
 })
-app.post('/mongooses', function(req, res){
-    var mongoose = new Mongoose({
-        name: req.body.name,
-        location: req.body.location,
-        skill: req.body.skill,
-        color: req.body.color,
-    })
-    mongoose.save(function(err){
-        if(err){
-            console.log("Couldn't insert document");
-        }else{
-            console.log('ADDED MONGOOSE - SUCCESS');
-            res.redirect('/');
-        }
+// Create
+app.post('/', function(req, res){
+    Mongoose.create(req.body, function(err, data){
+        if(err) {console.log(err);}
+        console.log(req.body);
+        res.redirect('/')
     })
 })
+// Update
 app.post('/mongooses/:id', function(req, res){
-    Mongoose.update({_id: req.params.id}, {$set: {
-        name: req.body.name,
-        location: req.body.location,
-        skill: req.body.skill,
-        color: req.body.color
-    }}, function(err, data){
-            res.redirect('/')
+    Mongoose.update({_id: req.params.id}, req.body, function(err, data){
+        if(err) {console.log(err);}
+        res.redirect('/')
     })
 })
+// Delete
 app.post('/mongooses/destroy/:id', function(req, res){
     Mongoose.remove({_id: req.params.id}, function(err, data){
         res.redirect('/')
     })
 })
 
-app.listen(3000, function(){
-    console.log('running on 3000');
+app.listen(port, function(){
+    console.log('running on ', port);
 })
